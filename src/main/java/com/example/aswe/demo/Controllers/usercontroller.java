eb72df587cdcbe800f56b78a5b753e057ee97ab8
khaled <khaledamrahmed0@gmail.com>
2023-03-11 14:42:15 +0200

    lab

--- src/main/java/com/example/aswe/demo/Controllers/usercontroller.java
+++ src/main/java/com/example/aswe/demo/Controllers/usercontroller.java
@@ -1,9 +1,12 @@
-package controller;
+package com.example.aswe.demo.Controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class usercontroller{
+public class usercontroller
+
+{
 @GetMapping("signup1")
 public String signupform(){
     return "signup.html";
@@ -11,8 +14,9 @@ public String signupform(){
 
 
 
-@GetMapping("home")
+@GetMapping("user")
 public String homepage(){
-    return "home.html";
+    return "Redirect:home.html";
 }
+    
 }
