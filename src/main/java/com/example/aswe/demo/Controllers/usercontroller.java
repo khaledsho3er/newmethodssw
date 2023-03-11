e3a19a702e4d5bc1ebef93f932102c7b09f12670
khaled <khaledamrahmed0@gmail.com>
2023-03-11 14:42:25 +0200

    lab

--- src/main/java/com/example/aswe/demo/Controllers/usercontroller.java
+++ src/main/java/com/example/aswe/demo/Controllers/usercontroller.java
@@ -2,11 +2,12 @@ package com.example.aswe.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class usercontroller
+public class usercontroller{
 
-{
+
 @GetMapping("signup1")
 public String signupform(){
     return "signup.html";
@@ -16,7 +17,8 @@ public String signupform(){
 
 @GetMapping("user")
 public String homepage(){
-    return "Redirect:home.html";
+    return "home.html";
+}
 }
     
-}
+
--- src/main/java/model/user.java
+++ src/main/java/model/user.java
@@ -2,7 +2,49 @@ package model;
 
 public class user {
 
+    private String fn;
+    private String ln;
+    private String email;
+    private String password;
+
+
+    public user(String fn, String ln, String email, String password) {
+        this.fn = fn;
+        this.ln = ln;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public String getFn() {
+        return fn;
+    }
+
+    public void setFn(String fn) {
+        this.fn = fn;
+    }
     
     
-    
+    public String getLn() {
+        return ln;
+    }
+
+    public void setLn(String ln) {
+        this.ln = ln;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
